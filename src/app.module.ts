819ac470370233923cdf09e8ebc4a0ffd1d0a93a
ignaciodz <ignaciodiazbr@gmail.com>
2025-11-19 17:25:36 -0500

    chore: install and configure TypeORM with datasource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from './data-source';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        ...AppDataSource.options,
+        autoLoadEntities: true,
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
