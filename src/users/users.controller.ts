1ce6fb870109d9e0889c820eae2910963c175eab
ignaciodz <ignaciodiazbr@gmail.com>
2025-11-29 22:23:00 -0500

    feat: add role-based access decorators and updateRole endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserRole } from '../common/enums/user-role.enum';
+import { UpdateUserRoleDto } from './dto/update-role-user.dto';
+import { Public } from 'src/auth/decorators/public.decorator';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Roles(UserRole.ADMIN)
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @Public()
   @Get(':id')
   findOneById(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.findOneById(id);
+  }
+
+  @Roles(UserRole.ADMIN)
+  @Patch(':id/role')
+  updateRole(@Param('id', ParseUUIDPipe) id: string, @Body() updateUserRole: UpdateUserRoleDto ){
+    return this.usersService.updateRole(id, updateUserRole);
   }
 
   @Patch(':id')
@@ -21,6 +33,7 @@ export class UsersController {
     return this.usersService.update(id, updateUserDto);
   }
 
+  @Roles(UserRole.ADMIN)
   @Delete(':id')
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.remove(id);
