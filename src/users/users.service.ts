56cb3a69f1aee5fcdaefd05d9bd03ad9b5a8189f
ignaciodz <ignaciodiazbr@gmail.com>
2025-11-29 22:27:56 -0500

    feat: add create, existsByEmail, update and updateRole methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
-import { UserRole } from './enums/user-role.enum';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserRoleDto } from './dto/update-role-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -11,6 +12,11 @@ export class UsersService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ){}
+
+  async create(createUserDto: CreateUserDto){
+    const user = this.userRepository.create(createUserDto);
+    return await this.userRepository.save(user);
+  }
 
   findAll() {
     return this.userRepository.find();
@@ -29,10 +35,27 @@ export class UsersService {
     return user;
   }
 
+  async existsByEmail(email: string){
+    return this.userRepository.existsBy({email});
+  }
+
   async update(id: string, updateUserDto: UpdateUserDto) {
+    const { email, ...restOfUpdates } = updateUserDto;
+
+    if (email) {
+      const existingUser = await this.userRepository.findOne({
+        where: { email },
+        select: ['id'],
+      });
+
+      if (existingUser && existingUser.id !== id) {
+        throw new ConflictException('Email already in use by another account.');
+      }
+    }
     const user = await this.userRepository.preload({
       id,
-      ...updateUserDto
+      email,
+      ...restOfUpdates,
     });
 
     if(!user) throw new NotFoundException(`User with ID ${id}, not found`);
@@ -40,15 +63,9 @@ export class UsersService {
     return this.userRepository.save(user);
   }
 
-  async updateRole(id: string, newRole: UserRole) {
-    const { role, ...userData } = await this.findOneById(id);
-
-    const userRoleUpdate = this.userRepository.create({
-      role: newRole,
-      ...userData
-    });
-
-    return this.userRepository.save(userRoleUpdate);
+  async updateRole(id: string, updateUserRoleDto: UpdateUserRoleDto) {
+    await this.userRepository.update(id, { role: updateUserRoleDto.role });
+    return this.userRepository.findOneBy({ id });
   }
 
   async remove(id: string) {
