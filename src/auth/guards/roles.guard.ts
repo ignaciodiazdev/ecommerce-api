d348152db3c7c845b7e6c92e4a187e2dbb84b130
ignaciodz <ignaciodiazbr@gmail.com>
2025-11-29 21:54:55 -0500

    feat: create RolesGuard for role-based access control

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,38 @@
+import { CanActivate, ExecutionContext, Injectable, ForbiddenException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+import { UserRole } from 'src/common/enums/user-role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(), 
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(ROLES_KEY, [
+      context.getHandler(), 
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles || requiredRoles.length === 0) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+    if (!user) throw new ForbiddenException('User not found');
+
+    const hasRequiredRole = requiredRoles.includes(user.role);
+    if (!hasRequiredRole) throw new ForbiddenException('You do not have the required role');
+
+    return true;
+  }
+}
