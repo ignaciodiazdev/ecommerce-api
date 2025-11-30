b50a45fed1ce332d24592f4b1d41169a27093bcb
ignaciodz <ignaciodiazbr@gmail.com>
2025-11-29 21:31:34 -0500

    feat: add configuration for Auth[Global Guards, ConfigModule] and import HashingModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,36 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from './data-source';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard, RolesGuard } from './auth/guards';
+import { HashingModule } from './common/hashing/hashing.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({isGlobal: true}),
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         ...AppDataSource.options,
         autoLoadEntities: true,
       })
     }),
-    UsersModule
+    UsersModule,
+    AuthModule,
+    HashingModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,  // JWT global
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,    // Roles global
+    },    
+  ],
 })
 export class AppModule {}
