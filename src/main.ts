f0ea485bd6d66cf0d12c9d9ab87bdbdf0ef75c44
ignaciodz <ignaciodiazbr@gmail.com>
2025-12-29 20:19:39 -0500

    feat: add swagger config & add global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +15,16 @@ async function bootstrap() {
       transform: true,
     })
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce API')
+    .setDescription('API para gestión del Ecommerce')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document);
   
   await app.listen(process.env.PORT ?? 3000);
 }
